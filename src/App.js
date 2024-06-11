66090a869f305f35c91444e31a019d5ec63d7896
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-06-12 00:30:54 +0530

    Somthing Went Wrong

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from './components/navbar';
 import Input from './components/input';
 import { useState } from 'react';
-// import About from './components/About';
+import Alert from './components/Alert';
 
 
 
@@ -9,18 +9,29 @@ import { useState } from 'react';
 
 function App() {
   const [Mode, SetMode]=useState('light');
+  const [Alert, SetAlert]=useState(null);
+
+  const ShowAlert=(message,type)=>{
+    SetAlert({
+      msg: message,
+      type: type
+    })
+  }
 
   const toggleFunction=()=>{
     if(Mode==='light'){
       SetMode('dark')
+      ShowAlert('Dark Mode Enabled', 'Sucess');
     }else{
-      SetMode('light')
+      SetMode('light');
+      ShowAlert('Light Mode Enabled', 'Sucess');
     }
   }
 
   return (
     <>
     <Navbar mode={Mode} toggleFunction={toggleFunction}/>
+    <Alert alert={Alert}/>
     <div className='container'>
       
       <Input  heading="Enter Text to Analyze"/>
